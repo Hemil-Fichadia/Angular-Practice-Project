7e2a41557b1bd06c87d146376f4408c73531d4bc
Hemil Fichadia <hemilfichadia@gamil.com>
2025-04-16 17:45:44 +0530

    First commit of angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+	selector: 'app-root',
+	imports: [RouterOutlet, HomeComponent, HeaderComponent],
+	template: `
+	<app-header/>
+	<main>
+		<app-home/>
+	</main>
   `,
-  styles: [],
+	styles: [`
+    main {
+		padding: 20px;
+	}
+    `],
 })
 export class AppComponent {
-  title = 'first-ng-app';
+	title = `Hemil's first angular app with app-root component`;
 }
